db887686da20fb85fe3bc8d399b4de1563986f5e
daohuytuan <tuandh95@gmail.com>
2023-12-10 23:56:21 +0700

    update countdown

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 import 'inter-ui/inter.css'
 import { WagmiProvider } from '@/contexts/WagmiProvider'
 import { ButtonMint } from '@/components/btn-mint'
+import { CountDown } from '@/containers/CountDown'
 
 export default function Home() {
   return (
     <WagmiProvider>
       <main>
         <div className="flex justify-center">
+          <CountDown />
           <ButtonMint />
         </div>
       </main>
--- containers/CountDown/index.tsx
+++ containers/CountDown/index.tsx
@@ -1,3 +1,37 @@
+import { ChangeEvent, useEffect, useState } from 'react'
+import { useCountdown } from 'usehooks-ts'
 export const CountDown = () => {
-  return <div className="flex"></div>
+  const [intervalValue, setIntervalValue] = useState<number>(1000)
+  const [count, { startCountdown, stopCountdown, resetCountdown }] =
+    useCountdown({
+      countStart: 30,
+      intervalMs: intervalValue
+    })
+
+  const handleChangeIntervalValue = (event: ChangeEvent<HTMLInputElement>) => {
+    setIntervalValue(Number(event.target.value))
+  }
+  useEffect(() => {
+    if (count === 0) {
+      resetCountdown()
+      startCountdown()
+    }
+  }, [count])
+  useEffect(() => {
+    startCountdown()
+  }, [])
+  return (
+    <div>
+      <p>Count: {count}</p>
+
+      <input
+        type="number"
+        value={intervalValue}
+        onChange={handleChangeIntervalValue}
+      />
+      <button onClick={startCountdown}>start</button>
+      <button onClick={stopCountdown}>stop</button>
+      <button onClick={resetCountdown}>reset</button>
+    </div>
+  )
 }
